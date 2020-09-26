938f4f33bab0aded2df06735ec8cf3f9073f12ae
daniel-junior-dube <daniel.junior.dube@gmail.com>
2020-09-26 13:13:36 -0400

    Improvement to collision and overall design

--- raylib_pong.c
+++ raylib_pong.c
@@ -9,13 +9,20 @@
 #define TARGET_FPS 60
 #define PANEL_MARGIN 32
 #define H1_FONT_SIZE 10
+#define BALL_SPEED 10.0f
 
-struct ObjectDimensions {
+struct Dimensions {
 	int screen_width;
 	int screen_height;
 	int paddle_width;
 	int paddle_height;
 	int ball_radius;
+};
+
+enum ScreenResolution {
+	R256X144,
+	R512X288,
+	R768X432,
 };
 
 enum Screen {
@@ -33,7 +40,7 @@ static const char *MAIN_MENU_OPTIONS_STRING[] = {
 };
 
 struct GlobalState {
-	struct ObjectDimensions object_dimensions;
+	struct Dimensions dimensions;
 	enum Screen current_screen;
 };
 
@@ -45,10 +52,16 @@ struct InGameState {
 	int player_1_score;
 	int player_2_score;
 	Vector2 ball_pos;
-	Vector2 ball_vel;
+	Vector2 ball_dir;
+	float ball_speed;
 	Rectangle left_paddle;
 	Rectangle right_paddle;
 };
+
+static const Vector2 UP_NORMAL = { 0.0f, 1.0f };
+static const Vector2 DOWN_NORMAL = { 0.0f, -1.0f };
+static const Vector2 RIGHT_NORMAL = { 1.0f, 0.0f };
+static const Vector2 LEFT_NORMAL = { -1.0f, 0.0f };
 
 void update_main_menu_screen(struct GlobalState* global_state, struct MainMenuState* state) {
 
@@ -101,37 +114,55 @@ struct Vector2 Vector2Reflect(struct Vector2 v, struct Vector2 l) {
 }
 
 void update_in_game_screen(struct GlobalState* global_state, struct InGameState* state) {
-	int screen_width = global_state->object_dimensions.screen_width;
-	int screen_height = global_state->object_dimensions.screen_height;
-	int paddle_height = global_state->object_dimensions.paddle_height;
-	int paddle_width = global_state->object_dimensions.paddle_width;
-	int ball_radius = global_state->object_dimensions.ball_radius;
+	int screen_width = global_state->dimensions.screen_width;
+	int screen_height = global_state->dimensions.screen_height;
+	int paddle_height = global_state->dimensions.paddle_height;
+	int paddle_width = global_state->dimensions.paddle_width;
+	int ball_radius = global_state->dimensions.ball_radius;
+
 	// ? Processing inputs
 	// ...
-	if (IsKeyDown(KEY_UP)) {
-		state->left_paddle.y -= 3.0f;
+	if (IsKeyDown(KEY_W)) {
+		state->left_paddle.y -= BALL_SPEED / 2.0f;
 		if (state->left_paddle.y < 0.0f) {
 			state->left_paddle.y = 0.0f;
 		}
 	}
-	if (IsKeyDown(KEY_DOWN)) {
-		state->left_paddle.y += 3.0f;
+	if (IsKeyDown(KEY_S)) {
+		state->left_paddle.y += BALL_SPEED / 2.0f;
 		float max_paddle_y = screen_height - paddle_height;
 		if (state->left_paddle.y > max_paddle_y) {
 			state->left_paddle.y = max_paddle_y;
 		}
 	}
+	if (IsKeyDown(KEY_UP)) {
+		state->right_paddle.y -= BALL_SPEED / 2.0f;
+		if (state->right_paddle.y < 0.0f) {
+			state->right_paddle.y = 0.0f;
+		}
+	}
+	if (IsKeyDown(KEY_DOWN)) {
+		state->right_paddle.y += BALL_SPEED / 2.0f;
+		float max_paddle_y = screen_height - paddle_height;
+		if (state->right_paddle.y > max_paddle_y) {
+			state->right_paddle.y = max_paddle_y;
+		}
+	}
 
 	// ? Updating
-	state->ball_pos = Vector2Add(state->ball_pos, state->ball_vel);
+
+	Vector2 ball_vec = Vector2Scale(state->ball_dir, state->ball_speed);
+
+	// Wall bounce
+	state->ball_pos = Vector2Add(state->ball_pos, ball_vec);
 	if ((int)state->ball_pos.y >= screen_height - ball_radius) {
-		Vector2 normal = { 0.0f, 1.0f };
-		state->ball_vel = Vector2Reflect(state->ball_vel, normal);
+		state->ball_dir = Vector2Reflect(state->ball_dir, UP_NORMAL);
 	}
 	if ((int)state->ball_pos.y <= ball_radius) {
-		Vector2 normal = { 0.0f, -1.0f };
-		state->ball_vel = Vector2Reflect(state->ball_vel, normal);
+		state->ball_dir = Vector2Reflect(state->ball_dir, DOWN_NORMAL);
 	}
+
+	// Point score
 	if ((int)state->ball_pos.x >= screen_width) {
 		++state->player_1_score;
 		state->ball_pos.x = (float)screen_width / 2.0f;
@@ -142,13 +173,102 @@ void update_in_game_screen(struct GlobalState* global_state, struct InGameState*
 		state->ball_pos.x = (float)screen_width / 2.0f;
 		state->ball_pos.y = (float)screen_height / 2.0f;
 	}
-	if (state->ball_vel.x < 0.0f && CheckCollisionCircleRec(state->ball_pos, ball_radius, state->left_paddle)) {
-		Vector2 normal = { 1.0f, 0.0f };
-		state->ball_vel = Vector2Reflect(state->ball_vel, normal);
+
+	// Paddle collision (reflect vel on paddle's normal)
+	if (ball_vec.x < 0.0f && CheckCollisionCircleRec(state->ball_pos, ball_radius, state->left_paddle)) {
+		
+		// above
+		if (state->ball_pos.y < state->left_paddle.y) {
+
+			// top-right
+			if (state->ball_pos.x > state->left_paddle.x + paddle_width) {
+				Vector2 top_right_corner = {
+					state->left_paddle.x + paddle_width,
+					state->left_paddle.y
+				};
+				state->ball_dir = Vector2Normalize(Vector2Subtract(state->ball_pos, top_right_corner));
+				state->ball_pos = Vector2Add(top_right_corner, Vector2Scale(state->ball_dir, ball_radius));
+
+			// top
+			} else {
+
+				// Reflect up vector
+				state->ball_dir = Vector2Reflect(state->ball_dir, UP_NORMAL);
+			}
+
+		// below
+		} else if (state->ball_pos.y > state->left_paddle.y + paddle_height) {
+
+			// bottom-right
+			if (state->ball_pos.x > state->left_paddle.x + paddle_width) {
+
+				// Check normalized vec between ball center and bottom-right corner
+				Vector2 bottom_right_corner = {
+					state->left_paddle.x + paddle_width,
+					state->left_paddle.y + paddle_height
+				};
+				state->ball_dir = Vector2Normalize(Vector2Subtract(state->ball_pos, bottom_right_corner));
+				state->ball_pos = Vector2Add(bottom_right_corner, Vector2Scale(state->ball_dir, ball_radius));
+			
+			// bottom
+			} else {
+
+				// Reflect down vector
+				state->ball_dir = Vector2Reflect(state->ball_dir, DOWN_NORMAL);
+			}
+
+		// right
+		} else {
+			state->ball_dir = Vector2Reflect(state->ball_dir, RIGHT_NORMAL);
+		}
 	}
-	if (state->ball_vel.x > 0.0f && CheckCollisionCircleRec(state->ball_pos, ball_radius, state->right_paddle)) {
-		Vector2 normal = { -1.0f, 0.0f };
-		state->ball_vel = Vector2Reflect(state->ball_vel, normal);
+	if (ball_vec.x > 0.0f && CheckCollisionCircleRec(state->ball_pos, ball_radius, state->right_paddle)) {
+		
+
+		// above
+		if (state->ball_pos.y < state->right_paddle.y) {
+
+			// top-left
+			if (state->ball_pos.x < state->right_paddle.x) {
+				Vector2 top_left_corner = {
+					state->right_paddle.x,
+					state->right_paddle.y
+				};
+				state->ball_dir = Vector2Normalize(Vector2Subtract(state->ball_pos, top_left_corner));
+				state->ball_pos = Vector2Add(top_left_corner, Vector2Scale(state->ball_dir, ball_radius));
+
+			// top
+			} else {
+
+				// Reflect up vector
+				state->ball_dir = Vector2Reflect(state->ball_dir, UP_NORMAL);
+			}
+
+		// below
+		} else if (state->ball_pos.y > state->right_paddle.y + paddle_height) {
+
+			// bottom-left
+			if (state->ball_pos.x < state->right_paddle.x) {
+
+				// Check normalized vec between ball center and bottom-right corner
+				Vector2 bottom_left_corner = {
+					state->right_paddle.x,
+					state->right_paddle.y + paddle_height
+				};
+				state->ball_dir = Vector2Normalize(Vector2Subtract(state->ball_pos, bottom_left_corner));
+				state->ball_pos = Vector2Add(bottom_left_corner, Vector2Scale(state->ball_dir, ball_radius));
+			
+			// bottom
+			} else {
+
+				// Reflect down vector
+				state->ball_dir = Vector2Reflect(state->ball_dir, DOWN_NORMAL);
+			}
+
+		// left
+		} else {
+			state->ball_dir = Vector2Reflect(state->ball_dir, LEFT_NORMAL);
+		}
 	}
 
 	// ? Rendering
@@ -171,25 +291,22 @@ void update_in_game_screen(struct GlobalState* global_state, struct InGameState*
 	EndDrawing();
 }
 
-enum ScreenResolution {
-	R256X144,
-	R512X288,
-};
-
-struct ObjectDimensions get_dimensions_for_resolution(enum ScreenResolution res) {
-	struct ObjectDimensions dim;
-	dim.screen_width = 256;
-	dim.screen_height = 144;
+struct Dimensions get_dimensions_for_resolution(enum ScreenResolution res) {
+	struct Dimensions dim;
+	int ratio = 1;
 	switch (res) {
 		case R512X288:
-			dim.screen_width *= 2;
-			dim.screen_height *= 2;
+			ratio = 2;
+			break;
+		case R768X432:
+			ratio = 3;
 			break;
 	}
-	int ratio = dim.screen_width / 256;
-	dim.paddle_width = ratio * 4;
+	dim.screen_width = ratio * 256;
+	dim.screen_height = ratio * 144;
+	dim.paddle_width = ratio * 6;
 	dim.paddle_height = ratio * 24;
-	dim.ball_radius = ratio * 3;
+	dim.ball_radius = ratio * 5;
 	return dim;
 }
 
@@ -203,12 +320,12 @@ int main(int argc, char **argv) {
 	SetConfigFlags(FLAG_MSAA_4X_HINT);
 	//SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 
-	struct ObjectDimensions dimensions = get_dimensions_for_resolution(R512X288);
+	struct Dimensions dimensions = get_dimensions_for_resolution(R768X432);
 	InitWindow(dimensions.screen_width, dimensions.screen_height, PROJECT_NAME);
 	SetTargetFPS(TARGET_FPS);
 
 	struct GlobalState global_state = {
-		.object_dimensions = dimensions,
+		.dimensions = dimensions,
 		.current_screen = MAIN_MENU_SCREEN,
 	};
 	struct MainMenuState main_menu_state = {
@@ -221,7 +338,8 @@ int main(int argc, char **argv) {
 			(float)dimensions.screen_width / 2.0f,
 			(float)dimensions.screen_height / 2.0f
 		},
-		.ball_vel = { 5.0f, 0.0f },
+		.ball_dir = LEFT_NORMAL,
+		.ball_speed = BALL_SPEED,
 		.left_paddle = {
 			PANEL_MARGIN,
 			dimensions.screen_height / 2 - dimensions.paddle_height / 2,
